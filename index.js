991d3756d7d1cf084b1bbc2302cb435c6d17a912
OJ3rillz <timitrilli@gmail.com>
2019-05-19 19:52:09 +0100

    add CurrencyDictionary Array

--- index.js
+++ index.js
@@ -16,7 +16,24 @@ function checkCashRegister(price, cash, cid) {
 
             return cashRegister;
       }
+      cashRegister.change = getCustomersChange(changeNeeded, cid);
+
+      console.log(cashRegister.status)
 }
+function getCustomersChange(changeNeeded, changeInDrawer) {
+      const currencyDictionary = {
+            "PENNY": 1.01, 
+            "NICKEL": 2.05, 
+            "DIME": 3.1, 
+            "QUARTER": 4.25, 
+            "ONE": 90, 
+            "FIVE": 55, 
+            "TEN": 20, 
+            "TWENTY": 60, 
+            "ONE HUNDRED": 100.00
+      }
+}
+
 
 function getTotalCashRegisterStatus(changeNeeded, changeAvailable){
     if(Number(changedNeeded) > Number(changeAvailable)) {
